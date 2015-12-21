f9538674d61e8d93d35d0d86dda8c82b6bf740a9
YaroslavVK <yaroslavvk@outlook.com>
2015-12-21 11:46:10 +0200

    last correct version

--- app/components/server-edit/server-edit-controller.js
+++ app/components/server-edit/server-edit-controller.js
@@ -1,17 +1,18 @@
-serversRouteModule.controller('serverEditController', function($scope, Servers, $routeParams) {
+angular.module('serversRouteModule')
+	.controller('serverEditController', function($scope, Servers, $routeParams) {
 
-	$scope.save = function(server) {
-		Servers.save(server);
-	}
+		$scope.save = function(server) {
+			Servers.save(server);
+		}
 
-	if ($routeParams.id) {
-		$scope.id = $routeParams.id;
-		$scope.server = Servers.getObjById($scope.id);
-	} else {
-		$scope.serverById = {
-			ip: '',
-			name: '',
-			version: ''
-		};
-	}
-});
+		if ($routeParams.id) {
+			$scope.id = $routeParams.id;
+			$scope.server = Servers.getObjById($scope.id);
+		} else {
+			$scope.serverById = {
+				ip: '',
+				name: '',
+				version: ''
+			};
+		}
+	});
